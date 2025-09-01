7b546d32002c8f93964425b04264b424d7368997
Sarah Rogue <189014817+SarahRogue81@users.noreply.github.com>
2025-08-31 20:06:08 -0400

    fixed categories and added Discord link

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,6 +24,7 @@ LINKS = (
 TWITTER_USERNAME = 'SarahRogue81'
 # Social widget
 SOCIAL = (
+    ('Discord', 'https://discord.com/users/sarahrogue.'),
     ('Twitter', f'https://x.com/{TWITTER_USERNAME}'),
 )
 SOCIAL_WIDGET_NAME = 'social media'
@@ -33,6 +34,7 @@ DEFAULT_PAGINATION = 4
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
+DEFAULT_CATEGORY = 'other'
 DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_PAGES_ON_MENU = False
 EXTRA_PATH_METADATA = {
@@ -45,4 +47,5 @@ MENUITEMS = (
 PLUGIN_PATHS = ['../pelican-plugins']
 PLUGINS = ['asciidoc_reader']
 THEME = 'notmyidea'
+USE_FOLDER_AS_CATEGORY = False
 
