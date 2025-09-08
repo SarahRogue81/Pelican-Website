f10180ebed9121eeb335e16bcd4a1c5192ad81aa
Sarah Rogue <189014817+SarahRogue81@users.noreply.github.com>
2025-09-08 07:19:27 -0400

    Publish site on 2025-09-08

--- theme/js/author-bio.js
+++
@@ -1,5 +0,0 @@
-window.onload = function () {
-    fetch('/author/sarah-rouge.html').then(response => response.text()).then(html => {
-        document.getElementById('include-pages').innerHTML = html;
-    });
-};
