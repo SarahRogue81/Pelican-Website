6b9b5b17db8d563ea9906e7b447964c99c3ffc93
Sarah Rogue <189014817+SarahRogue81@users.noreply.github.com>
2025-10-22 15:17:50 +0000

    changes made on 22-10-2025

--- gems.sh
+++ gems.sh
@@ -1,6 +1,6 @@
 # source this file to install the asciidoctor gem
 # used to install the asciidoctor gem on replit.com
 
-export PATH=$PATH:$ASCIIDOC_DIR
+export PATH=$PATH:$ASCIIDOCTOR_DIR
 
 gem install asciidoctor
